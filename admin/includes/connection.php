3eb48d2247409ea19dc671128e5413e79cf3875b
Mohammad-404 <74400046+Mohammad-404@users.noreply.github.com>
2021-01-08 21:12:03 +0200

    Admin
    Update 1/8/2021 9:11

--- admin/includes/connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost","root","","ecom6");
-    if(!$conn){
-        die("cannot connect ot server");
-    }
--- admin/manage_category.php
+++
@@ -1,2 +0,0 @@
-<?php include('includes/admin_header.php'); ?>
-<?php include('includes/admin_footer.php'); ?>
